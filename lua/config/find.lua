275272d1844a328154614c1240d6bd885aa02e9b
jarvarvarvis <jarvistrigo@gmail.com>
2024-08-06 21:04:36 +0200

    Clean up find.lua and fix keybinds

--- lua/config/find.lua
+++ lua/config/find.lua
@@ -1,10 +1,10 @@
 local wk = require('which-key')
-local builtin = require('telescope.builtin')
+local telescope_builtin = require('telescope.builtin')
 
 wk.add({
     { "<space>f",  group = "find" },
-    { "<space>fb", builtin.find_files, desc = "Find buffers" },
-    { "<space>ff", builtin.buffers,    desc = "Find files" },
-    { "<space>fg", builtin.oldfiles,   desc = "Live Grep" },
-    { "<space>fr", builtin.live_grep,  desc = "Recent files" },
+    { "<space>fb", telescope_builtin.buffers, desc = "Find buffers" },
+    { "<space>ff", telescope_builtin.find_files,    desc = "Find files" },
+    { "<space>fg", telescope_builtin.live_grep,   desc = "Live Grep" },
+    { "<space>fr", telescope_builtin.oldfiles,  desc = "Recent files" },
 })
