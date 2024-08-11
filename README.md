a5a172bcd4e1f3068f1d24f588865124a2358d98
jarvarvarvis <jarvistrigo@gmail.com>
2024-08-11 20:01:44 +0200

    A little hacking around on the config (add Vim-esque code folding, ...), rust-analyzer dependency instructions in README.md

--- README.md
+++ README.md
@@ -8,5 +8,6 @@ Clone this repository into your Neovim configuration path (on most Linux distrib
 Before running Neovim, you need to install the following dependencies:
 - npm (Language servers)
 - ripgrep (`Telescope live_grep`)
+- Rust source code (needed for full completions, `rustup component add rust-src`)
 
 Then, start Neovim using `nvim` in the terminal or in any other way.
--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -8,7 +8,19 @@ require('nvim-treesitter.configs').setup {
         "rust",
         "python",
     },
+    auto_install = true,
     highlight = {
-        enable = true
+        enable = true,
+        additional_vim_regex_highlighting = false,
+    },
+    ident = { enable = true },
+    rainbow = {
+        enable = true,
+        extended_mode = true,
+        max_file_lines = nil,
     }
 }
+
+-- Treesitter folding
+vim.wo.foldmethod = 'expr'
+vim.wo.foldexpr = 'nvim_treesitter#foldexpr()'
